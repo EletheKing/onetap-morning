d98b561dc1bc1492f5167922fb3168ea231ae665
EletheKing <164313797+EletheKing@users.noreply.github.com>
2025-08-10 16:37:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(r => r || fetch(event.request))
+  );
+});
