4f112846bfd53c9f725f529d3476159f40253cf6
EletheKing <164313797+EletheKing@users.noreply.github.com>
2025-08-10 16:47:09 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(r => r || fetch(event.request))
-  );
-});
